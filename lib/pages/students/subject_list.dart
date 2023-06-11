f1981dd0f585f1c65ffb50be293627a1f737ab1f
Krishna Prasad A <kpa.krishnaprasada@gmail.com>
2023-06-11 14:09:58 +0530

    common app bar added
    the changes in parameters for calling common app bar also done

--- lib/pages/students/subject_list.dart
+++ lib/pages/students/subject_list.dart
@@ -1,19 +1,22 @@
+import 'package:e_class/pages/common%20widgets/page_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:e_class/pages/common widgets/subject_buttons.dart';
 import 'package:e_class/data/subjects.dart';
 
 // ignore: must_be_immutable
 class SubjectList extends StatelessWidget{
-  SubjectList(this.id,this.schemeId,{super.key});
+  SubjectList(this.sem,this.schemeId,this.userId,this.userType, {super.key});
 
-  int id;
+  int sem;
   int schemeId;
+  String userId;
+  int userType;
 
-  List<Widget> showSubjects(schemeValue,semId){
+  List<Widget> showSubjects(schemeValue,semId,userId,userType){
     List<Widget> subjectsAvailable = [];
     for(var i =0;i<subjectList.length;i++){
       if(subjectList[i].schemeId == schemeValue && subjectList[i].semester == semId){
-        subjectsAvailable.add(SubjectButton(subjectList[i].id,subjectList[i].subjectName));
+        subjectsAvailable.add(SubjectButton(subjectList[i].id,subjectList[i].subjectName,userId,userType));
       }
     }
     return subjectsAvailable;
@@ -21,17 +24,20 @@ class SubjectList extends StatelessWidget{
 
   @override
   Widget build(context){
-    return  SizedBox(
-      child: DecoratedBox(
-        decoration: const BoxDecoration(
-          color: Colors.white
+    return  Scaffold(
+      appBar: CommonAppBar("Subjects", 'S$sem', userType, userId),
+      body: SizedBox(
+        child: DecoratedBox(
+          decoration: const BoxDecoration(
+            color: Colors.white
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [const SizedBox(height: 100,width: 10,),...showSubjects(schemeId,sem,userId,userType)],
+              ),),
         ),
-        child: SingleChildScrollView(
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [const SizedBox(height: 100,width: 10,),...showSubjects(schemeId,id)],
-            ),),
       ),
     );
   }
